0362c89207136afbc6e0f265ca647d526a355ce0
Umesh Mohan <mohan.umesh@gmail.com>
2016-01-03 19:31:23 +0530

    Create common.hpp

---
+++ common.hpp
@@ -0,0 +1,16 @@
+#ifndef __Common_included__
+#define __Common_included__
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <opencv2/core.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+
+using namespace std;
+
+#endif
